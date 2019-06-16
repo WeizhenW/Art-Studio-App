d5fe9e141e0202d7153ceff1ff7ed4cbb8b1da63
Weizhen Wang <wangwzhen@gmail.com>
2019-06-16 14:23:01 -0500

    create material ui table on cart page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,15 +5,49 @@ import CartItem from '../CartItem/CartItem';
 //router
 import { Link } from 'react-router-dom';
 
+//material ui
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import Button from '@material-ui/core/Button';
+
+
+const styles={
+    table: {
+        maxWidth: "80%",
+        margin: "20px auto",
+    },
+    nextPage: {
+        height: "50px",
+        maxWidth: "200px",
+        marginBottom: "100px",
+    },
+    total: {
+        fontSize: "20px",
+        textAlign: "right",
+        marginRight: "150px",
+    }
+}
 class Cart extends Component {
 
     
     render() {
         return(
             <div>
-                {this.props.reduxState.cartReducer.map(painting => <CartItem key={painting.id} painting={painting} />)}
-                <h4>
-                    Total: 
+                <Table style={styles.table}>
+                    <TableHead>
+                        <TableCell>Art Image</TableCell>
+                        <TableCell>Art Name</TableCell>
+                        <TableCell>Art Description</TableCell>
+                        <TableCell>Art Price</TableCell>
+                    </TableHead>
+                    <TableBody>                
+                    {this.props.reduxState.cartReducer.map(painting => <CartItem key={painting.id} painting={painting} />)}
+                    </TableBody>
+                </Table>
+                <h4 style={styles.total}>
+                    Total: $
                     {this.props.reduxState.cartReducer.length === 0? 
                         0 
                         : 
@@ -22,7 +56,7 @@ class Cart extends Component {
                         ).price
                     }
                 </h4>
-                <Link to="/customerinfo"><button>Next Step</button></Link>
+                <Link to="/customerinfo"><Button variant="contained" style={styles.nextPage} color="secondary">Go to Checkout</Button></Link>
             </div>
         )
     }
--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+//material ui
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { height } from '@material-ui/system';
+
 class CartItem extends Component {
     
     render() {
         return(
-            <div>
-                <li>
-                {this.props.painting.name} - 
-                {this.props.painting.description} -
-                {this.props.painting.price}
-                </li>
-            </div>
+            
+                <TableRow>
+                    <TableCell>
+                        <img src={this.props.painting['image_path'] } height="100px"/>
+                    </TableCell>
+                    <TableCell>                
+                        {this.props.painting.name}
+                    </TableCell>
+                    <TableCell>
+                        {this.props.painting.description}
+                    </TableCell>
+                    <TableCell>
+                        {this.props.painting.price}
+                    </TableCell>
+                </TableRow>
         )
     }
 }
