7f6e04017fee948d874da971f9c55d0cc5866e10
Weizhen Wang <wangwzhen@gmail.com>
2019-06-16 16:35:05 -0500

    add introduction in the left panel

--- src/components/LeftPanel/LeftPanel.js
+++ src/components/LeftPanel/LeftPanel.js
@@ -6,59 +6,58 @@ import { Link as RouterLink } from 'react-router-dom';
 
 //material ui
 import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
 import Avatar from '@material-ui/core/Avatar';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
 
 
 const styles = {
-    avatar: {
-        marginTop: 50,
-        justify: "center",
+    avatar: {        
+        margin: "50px  auto",
         width: 200,
         height: 200,
     },
     ul: {
         marginTop: 50,
         color: "grey",
+    },
+    paper: {
+        marginTop: 0,
+        marginRight: 30,
+    },
+    typography: {
+        marginTop: 30,
+        padding: 10,
     }
-    
+
 }
 
 class LeftPanel extends Component {
 
     render() {
         return (
-                    <Grid container  item xs={12} md={3}>
-                        <Avatar
-                            style={styles.avatar}
-                            src="images/cards/logo.jpeg"
-                            alt="logo"
-                        />
-                       
-                        
-                        {/* <ul>
-                            <li>
-                                <Link to="/artlist" component={RouterLink}>
-                                    Art List
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/cart" component={RouterLink}>
-                                    Cart
-                                 </Link>
-                            </li>
-                            <li>
-                                <Link to="/customerinfo" component={RouterLink}>
-                                    Customer Information
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="summary" component={RouterLink}>
-                                    Purchase Summary
-                                </Link>
-                            </li>
-                        </ul> */}
-                    </Grid>          
+            <Grid container item xs={12} md={3}>                
+                <Paper style={styles.paper}>
+                    <Avatar
+                        style={styles.avatar}
+                        src="images/cards/logo.jpeg"
+                        alt="logo"
+                    />
+
+                    <br />
+
+                    <Typography style={styles.typography} variant="h5" component="h3">
+                        About me:
+                    </Typography>
+                    <Typography style={styles.typography} component="p">
+                    I was a chemistry student. After working in industry for a while, I struggle with myself for a really long time. I try to get my mind straight and I finally realized that I want to create in terms of painting and drawing. Although I know that I start it late, I want to keep moving forward. Even just a little bit everyday. I welcome you all to visit my website at http://yuezengartstudio.com I share my thoughts and tips for art and crafts. Thank you very much~
+
+                    </Typography>
+                </Paper>
+
+
+            </Grid>
         )
     }
 }
