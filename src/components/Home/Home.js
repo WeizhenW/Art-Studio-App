9a9c4ca018756fe7f390ca44ce15882c3283d32b
Weizhen Wang <wangwzhen@gmail.com>
2019-06-16 13:32:51 -0500

    add material ui to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,63 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
+//material ui
+import Grid from '@material-ui/core/Grid'
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+
+const styles = {
+    card: {
+        maxWidth: "75%",
+        margin: "60px auto",
+    }
+}
 
 class Home extends Component {
     render() {
         return(
             <div>
-                <ul>
-                    <li>                    
-                        <Link to="/artlist">My Art List</Link>
-                    </li>
-                    <li>                    
-                        <Link to="/blogs">My Blog</Link>
-                    </li>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={6}>
+                        <Card style={styles.card}>
+                            <CardHeader
+                                title="My Art List"
+                            />
+                            <CardMedia
+                                component="img"
+                                image="images/cards/boat.jpg"
+                                title="boat"
+                            />
+                            <CardActions disableSpacing>
+                                <Link to="/artlist">
+                                    <Button size="small">Enter</Button>
+                                </Link>
+                            </CardActions>
+                        </Card>
+                    </Grid>
 
-                </ul>
-              
+                    <Grid item xs={12} md={6}>
+                        <Card style={styles.card}>
+                            <CardHeader
+                                title="My Blogs"
+                            />
+                            <CardMedia
+                                component="img"
+                                image="images/cards/blogs.png"
+                                title="blogs"
+                            />
+                            <CardActions disableSpacing>
+                                <Link to="/blogs">
+                                    <Button size="small">Enter</Button>
+                                </Link>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                </Grid>
+                
             </div>
         )
     }
